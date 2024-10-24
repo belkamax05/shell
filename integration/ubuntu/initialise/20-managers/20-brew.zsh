6ba1bba814afa0f612ed0a0e177ae65da169fa52
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-10-25 00:34:22 +0100

    latest

--- integration/ubuntu/initialise/20-managers/20-brew.zsh
+++ integration/ubuntu/initialise/20-managers/20-brew.zsh
@@ -1,7 +1,9 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-if ! grep -Fxq 'eval "$($(brew --prefix)/bin/brew shellenv)"' ~/.bashrc; then
-    echo 'eval "$($(brew --prefix)/bin/brew shellenv)"' >> ~/.bashrc
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    if ! grep -Fxq 'eval "$($(brew --prefix)/bin/brew shellenv)"' ~/.bashrc; then
+        echo 'eval "$($(brew --prefix)/bin/brew shellenv)"' >> ~/.bashrc
+    fi
 fi
