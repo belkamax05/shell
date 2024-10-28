35a228fa837c0800304a314462baca6a76b1a30b
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-10-28 04:31:51 +0000

    latest

--- configs/install-optional.zsh
+++ configs/install-optional.zsh
@@ -2,16 +2,10 @@ echo-info "Installing ${COLOR_CYAN}OPTIONAL${STYLE_RESET} packages"
 _installPackages=""
 if s-run is linux; then
     _installPackages="$_installPackages build-essential procps twingate chrome java"
-
     _installPackages="$_installPackages gnome-browser-connector gnome-tweaks gnome-shell-extension-installer gnome-shell-extension-prefs"
-
     _installPackages="$_installPackages ulauncher teams-for-linux whatsapp-linux-app raindrop slack spotify"
-
     _installPackages="$_installPackages docker-cli docker-desktop"
-
-
-    _installPackages="$_installPackages gnome-shell-extension-installer
-    gnome-shell-extension-prefs "
+    _installPackages="$_installPackages gnome-shell-extension-installer gnome-shell-extension-prefs "
 fi
 
 packages_array=(${=_installPackages})
