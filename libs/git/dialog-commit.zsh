5b56fd0a0af8191317ff75558b3e96d8d3dc4dc9
Maksym Bondarchuk <belkamax05@gmail.com>
2024-10-29 01:50:35 +0000

    latest

--- libs/git/dialog-commit.zsh
+++ libs/git/dialog-commit.zsh
@@ -1,17 +1,15 @@
 shell-git-dialog-commit() {
-    # local branch=$(shell-git-current-branch)
-    # local commit=$(shell-git-current-commit)
-    # local message=$(dialog --stdout --inputbox "Commit message" 0 0 "$commit")
-    # if [ -n "$message" ]; then
-    #     shell-git-fast-commit "$message"
-    # fi
     _commitMessage="$1"
+    if [[ $_commitMessage == "" ]]; then
+        _commitMessage=$(gum input --header "Commit message")
+    fi
     if [[ "$@" == *"--flow"* ]]; then
         _commitFlowMode=$(gum choose "feat" "fix" "hotfix" --header "GitFlow Commit mode")
         _commitMessage=$(gum input --header "Commit message" --value="DFS2-00000: $_commitFlowMode: $_commitMessage")
     fi
+    _stagedCount=$(s-run git staged-files-count)
     s-run git me
-    echo-warning "Commit message is ${COLOR_ARGUMENT}$_commitMessage${STYLE_RESET}"
+    echo-warning "Commit ${COLOR_ARGUMENT}$_stagedCount${STYLE_RESET} files with message ${COLOR_ARGUMENT}$_commitMessage${STYLE_RESET}"
     if gum confirm "Do you want to commit?"; then
         s-run git fast-commit $_commitMessage
         s-run git me
