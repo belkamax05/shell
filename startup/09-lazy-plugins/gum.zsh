d0ddb474e11e99c2b3fefb78d79066abbfdb502b
Maksym Bondarchuk <belkamax05@gmail.com>
2024-11-28 22:50:17 +0000

    latest

--- startup/09-lazy-plugins/gum.zsh
+++ startup/09-lazy-plugins/gum.zsh
@@ -1,11 +1,11 @@
-tracing "Gum loads"
-if [[ $(command -v gum) == "" ]]; then
-    s-debug info "Lazy loading ${COLOR_SUCCESS}gum${STYLE_RESET}"
-    gum() {
-        unset -f gum
-        s-debug info "Installing ${COLOR_SUCCESS}gum${STYLE_RESET}"
-        brew install gum > /dev/null
-        gum $@
-    }
-fi
-tracing "Gum loaded"
+# tracing "Gum loads"
+# if [[ $(command -v gum) == "" ]]; then
+#     s-debug info "Lazy loading ${COLOR_SUCCESS}gum${STYLE_RESET}"
+#     gum() {
+#         unset -f gum
+#         s-debug info "Installing ${COLOR_SUCCESS}gum${STYLE_RESET}"
+#         brew install gum > /dev/null
+#         gum $@
+#     }
+# fi
+# tracing "Gum loaded"
