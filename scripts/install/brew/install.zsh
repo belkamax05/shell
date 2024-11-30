4c7248dc49b194458b142c93cc4f4cc87f20d28b
Maksym Bondarchuk <belkamax05@gmail.com>
2024-11-30 04:00:35 +0000

    latest

--- scripts/install/brew/install.zsh
+++
@@ -1,9 +0,0 @@
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    s-run plugin-brew-init
-    
-    if ! grep -Fxq 'eval "$($(brew --prefix)/bin/brew shellenv)"' ~/.bashrc; then
-        echo 'eval "$($(brew --prefix)/bin/brew shellenv)"' >> ~/.bashrc
-    fi
-fi
